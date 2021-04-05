fed0c5eb2268f6c4b5adc0b90e12d959edf02bab
Vinicius Catarino <vinicius_catarino@outlook.com>
2021-04-05 14:16:08 -0300

    Dependency Inversion Principle

--- src/modules/appointments/http/routes/appointments.routes.ts
+++ src/modules/appointments/http/routes/appointments.routes.ts
@@ -1,29 +1,30 @@
 import { Router } from "express";
-import { getCustomRepository } from "typeorm";
 import { parseISO } from "date-fns";
 
 import CreateAppointmentService from "@modules/appointments/services/CreateAppointmentService";
+
 import AppointmentsRepository from "@modules/appointments/infra/typeorm/repositories/AppointmentsRepository";
 
 import ensureAuthenticated from "@modules/users/infra/http/middlewares/ensureAuthenticated";
 
 const appointmentsRouter = Router();
 
+const appoinmentsRepository = new AppointmentsRepository();
+
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.get("/", async (request, response) => {
-  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-  const appointments = await appointmentsRepository.find();
+// appointmentsRouter.get("/", async (request, response) => {
+//   const appointments = await appointmentsRepository.find();
 
-  return response.json(appointments);
-});
+//   return response.json(appointments);
+// });
 
 appointmentsRouter.post("/", async (request, response) => {
   const { provider_id, date } = request.body;
 
   const parsedDate = parseISO(date);
 
-  const CreateAppointment = new CreateAppointmentService();
+  const CreateAppointment = new CreateAppointmentService(appoinmentsRepository);
 
   const appointment = await CreateAppointment.execute({
     date: parsedDate,
--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,22 +1,23 @@
 import { startOfHour } from "date-fns";
-import { getCustomRepository } from "typeorm";
 
 import Appointment from "../infra/typeorm/entities/Appointment";
-import AppointmentsRepository from "../infra/typeorm/repositories/AppointmentsRepository";
+
+import IAppointmentsRepository from "../repositories/IAppointmentsRepository";
 
 import AppError from "@shared/errors/AppError";
 
-interface Request {
+interface IRequest {
   provider_id: string;
   date: Date;
 }
 
 class CreateAppointmentService {
-  public async execute({ date, provider_id }: Request): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  constructor(private appoinmentsRepository: IAppointmentsRepository) {}
+
+  public async execute({ date, provider_id }: IRequest): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await this.appoinmentsRepository.findByDate(
       appointmentDate
     );
 
@@ -24,12 +25,10 @@ class CreateAppointmentService {
       throw new AppError("This appointment is alredy booked");
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = await this.appoinmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
-
-    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
