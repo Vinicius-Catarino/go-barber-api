5d2b0314f4bf070df84e39bda37532a40eab5567
Vinicius Catarino <vinicius_catarino@outlook.com>
2021-04-12 13:02:53 -0300

    Added test in appointments

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from "@shared/errors/AppError";
 import FakeAppointmentsRepository from "../repositories/fakes/FakeAppointmentsRepository";
 import CreateAppointmentService from "./CreateAppointmentService";
 
@@ -18,5 +19,24 @@ describe("CreateAppointment", () => {
     expect(appointment.provider_id).toBe("123123");
   });
 
-  // it("should not be able to create two appointments on the same time", () => {});
+  it("should not be able to create two appointments on the same time", async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository
+    );
+
+    const appointmentDate = new Date(2021, 4, 12, 11);
+    const appointment = await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: "123123",
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: "123123",
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
